39466755a0d29eaa8173cf61b76ea0fd2632e2f2
BSKOM <b.shelestua@gmail.com>
2021-07-18 19:11:13 +0300

    feat: add function decode

--- src/index.js
+++ src/index.js
@@ -38,6 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
+  function decode(expr) {
+    const arr = expr.split('')
+    const okey = Object.keys(MORSE_TABLE)
+    const oval = Object.values(MORSE_TABLE)
+    let acc = ''
+    let sarr = []
+    let marr = []
+    console.log(oval)
+    for (let i = 0; i < arr.length; i++) {
+      acc += arr[i]
+      if (((i + 1) % 10) === 0) {
+        console.log('acc', acc, acc.indexOf('1'))
+        sarr[sarr.length] = acc.substring(acc.indexOf('1')).
+          replace(/10/g, '.').replace(/11/g, '-').replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ')
+        acc = ''
+      }
+    }
+    for (let i = 0; i < sarr.length; i++) {
+      let n = okey.indexOf(sarr[i])
+      if (n >= 0) {
+        marr[i] = oval[n]
+      } else {
+        marr[i] = sarr[i]
+      }
+    }
+    return marr.join('')
+  }
     // write your solution here
 }
 
